b130e6f442a67d43fa654bf702b8bf7a63148b7c
Malak Mehrez <malakmehrez2@gmail.com>
2022-11-19 11:52:16 +0200

    Successfully downloaded frame added

--- src/AudiosFrame.java
+++ src/AudiosFrame.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.JButton;
@@ -9,79 +11,109 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class AudiosFrame {
-    
-    public static void create(){
+
+    public static void create() {
         JFrame frame = new JFrame("Songs");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(100, 100, 800, 500);
-		frame.setResizable(false);
-		JPanel contentPane = new JPanel();
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(0, 24, 35, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		frame.setContentPane(contentPane);
-		contentPane.setLayout(null);
+        frame.setBounds(100, 100, 800, 500);
+        frame.setResizable(false);
+        JPanel contentPane = new JPanel();
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(0, 24, 35, 255));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        frame.setContentPane(contentPane);
+        contentPane.setLayout(null);
         frame.setVisible(true);
 
         JLabel lblFileName = new JLabel("File Name");
-		lblFileName.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+        lblFileName.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
         lblFileName.setForeground(new Color(168, 207, 69, 255));
-		lblFileName.setBounds(10, 20, 250, 20);
-		contentPane.add(lblFileName);
-		
-		JLabel lblFileUploader = new JLabel("File Uploader");
+        lblFileName.setBounds(10, 20, 250, 20);
+        contentPane.add(lblFileName);
+
+        JLabel lblFileUploader = new JLabel("File Uploader");
         lblFileUploader.setForeground(new Color(168, 207, 69, 255));
-		lblFileUploader.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
-		lblFileUploader.setBounds(270, 20, 250, 20);
-		contentPane.add(lblFileUploader);
-		
-		JLabel lblDownload = new JLabel("File Download");
-		lblDownload.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
-		lblDownload.setBounds(530, 20, 250, 20);
+        lblFileUploader.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+        lblFileUploader.setBounds(270, 20, 250, 20);
+        contentPane.add(lblFileUploader);
+
+        JLabel lblDownload = new JLabel("File Download");
+        lblDownload.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+        lblDownload.setBounds(530, 20, 250, 20);
         lblDownload.setForeground(new Color(168, 207, 69, 255));
-		contentPane.add(lblDownload);
-        
-        File dir = new File("C:\\Users\\Malak\\Desktop\\AUB\\FALL 2022\\CMPS 242\\project\\srcCode\\MUZE\\src\\audios");
-        if(dir.isDirectory()){
-            File[] files = dir.listFiles();
+        contentPane.add(lblDownload);
+
+        File audios = new File("C:\\Users\\Malak\\Desktop\\AUB\\FALL 2022\\CMPS 242\\project\\srcCode\\MUZE\\src\\audios");
+        File  downloads = new File("C:\\Users\\Malak\\Desktop\\AUB\\FALL 2022\\CMPS 242\\project\\srcCode\\MUZE\\src\\downloads");
+        File[] files = audios.listFiles();
             int height = 30;
-            for(File file: files){
+            for (File file : files) {
                 String fileName = file.getName();
-                if(fileName.endsWith(".mp3")){
+                if (fileName.endsWith(".mp3")) {
                     String fileNameNoExt = getFileNameWithoutExtension(fileName);
-                    String[] audioFile = fileNameNoExt.split("-"); 
+                    String[] audioFile = fileNameNoExt.split("-");
 
                     JLabel lblFileName1 = new JLabel(audioFile[1]);
                     lblFileName1.setFont(new Font("Tahoma", Font.PLAIN, 16));
                     lblFileName1.setForeground(new Color(168, 207, 69, 255));
-                    lblFileName1.setBounds(10, 20+height, 250, 20);
+                    lblFileName1.setBounds(10, 20 + height, 250, 20);
                     contentPane.add(lblFileName1);
 
                     JLabel lblFileUploader1 = new JLabel(audioFile[0]);
                     lblFileUploader1.setForeground(new Color(168, 207, 69, 255));
                     lblFileUploader1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-                    lblFileUploader1.setBounds(270, 20+height, 250, 20);
+                    lblFileUploader1.setBounds(270, 20 + height, 250, 20);
                     contentPane.add(lblFileUploader1);
-                    
+
                     JButton btnDownload1 = new JButton("Download");
-                    btnDownload1.setBackground(new Color(168,207,69,255));
-                    btnDownload1.setBounds(540, 20+height, 95, 23);
+                    btnDownload1.setBackground(new Color(168, 207, 69, 255));
+                    btnDownload1.setBounds(540, 20 + height, 95, 23);
+                    btnDownload1.addActionListener(new ActionListener(){
+                        @Override
+                        public void actionPerformed(ActionEvent e){
+                            try {
+                                Thread.sleep(10000);
+                                if(indirectory(downloads, file)){
+                                    SuccessfulDownloadFrame.create();
+                                }
+
+                            } catch (InterruptedException e1) {
+                                // TODO Auto-generated catch block
+                                e1.printStackTrace();
+                            }
+                        }
+                    });
                     contentPane.add(btnDownload1);
 
-                    height+=30;
-                    
+                    height += 30;
+
+                
+            }
+        }
+
+    }
+
+    public static String getFileNameWithoutExtension(String filename) {
+        String fileName = "";
+        for (int i = 0; i < filename.length(); i++) {
+            if (filename.charAt(i) == '.') {
+                break;
+            }
+            fileName += filename.charAt(i);
+        }
+        return fileName;
+    }
+
+    public static boolean indirectory(File audiosFile, File fileName) {
+        if (audiosFile.isDirectory()) {
+            File[] files = audiosFile.listFiles();
+            for (File file : files) {
+                if (file.getName().equals(fileName.getName())) {
+                    return true;
                 }
             }
         }
-       
+        return false;
     }
-    private static String getFileNameWithoutExtension(String filename){
-        String fileName = "";
-        for(int i = 0;i<filename.length();i++){
-            if(filename.charAt(i) == '.') {break;}
-                fileName+=filename.charAt(i);
-            }
-        return fileName;
-    }
-    
+
 }
---
+++ src/SuccessfulDownloadFrame.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.border.EmptyBorder;
+
+public class SuccessfulDownloadFrame {
+    public static void create() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 300, 150);
+        JPanel contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        frame.setContentPane(contentPane);
+        frame.setVisible(true);
+        contentPane.setLayout(null);
+
+        JLabel lblDownSuccess = new JLabel("Downloaded Successfully!");
+        lblDownSuccess.setFont(new Font("Tahoma", Font.BOLD, 16));
+        lblDownSuccess.setBounds(33, 22, 221, 31);
+        contentPane.add(lblDownSuccess);
+
+        JButton btnOK = new JButton("OK");
+        btnOK.addActionListener((ActionListener) new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        btnOK.setBounds(165, 79, 89, 23);
+        contentPane.add(btnOK);
+    }
+
+}
