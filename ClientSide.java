3a680ae5ef2fc5cdf270f186b2500bae1ad3a6d9
monaghorayeb <93760987+monaghorayeb@users.noreply.github.com>
2022-11-23 16:13:12 +0200

    Update ClientSide.java

--- ClientSide.java
+++ ClientSide.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Client {
+public class ClientSide {
     
     public static void main(String[] args) throws UnknownHostException, IOException {
         
