456e650e4872955136084609eaa3b6e2b0aba5c4
monaghorayeb <93760987+monaghorayeb@users.noreply.github.com>
2022-11-28 15:25:33 +0200

    client and server edited

--- src/client.java
+++ src/client.java
@@ -1,15 +1,14 @@
-package backend;
-import FrontEnd0.*;
-
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class client {
     public static void main(String[] args) throws IOException{
-        sendFiletoServer(new File("C:\\Users\\Malak\\Downloads\\songs.jpg"));
+        sendFiletoServer(new File("C:\\Users\\Lenovo\\Desktop\\Cmps 242\\Project\\Listen.jpeg"));
     }
-    public static void sendFiletoServer(File file) throws UnknownHostException, IOException{
+
+
+    public static void sendFiletoServer(File file) throws UnknownHostException, IOException {
         Socket socket = new Socket("localhost", 2222);
         DataInputStream daInputStream = new DataInputStream(socket.getInputStream());
         DataOutputStream daOutputStream = new DataOutputStream(socket.getOutputStream());
@@ -19,13 +18,14 @@ public class client {
         
         byte[] buffer = new byte[4*1024];
         int bytes = fileInputStream.read(buffer);
+
         while(bytes!=-1){
             daOutputStream.write(buffer, 0, bytes);
             daOutputStream.flush();
         }
+
         daInputStream.close();
         daOutputStream.close();
     }
-
     
 }
--- src/server.java
+++ src/server.java
@@ -1,5 +1,3 @@
-package backend;
-
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,31 +9,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import FrontEnd0.HomeFrame;
-
 public class server {
     public static void main(String[] args) throws IOException{
-        receiveFilefromClient(new File("C:\\Users\\Malak\\Downloads\\listen.jpg"));
+        receiveFilefromClient(new File("C:\\Users\\Lenovo\\Desktop\\Cmps 242\\Project\\Listen.jpeg"));
     }
+
+    
     public static void receiveFilefromClient(File file) throws IOException {
         ServerSocket serverSocket = new ServerSocket(2222);
         System.out.println("Server is Starting in port 2222");
         Socket clientSocket = serverSocket.accept();
         System.out.println("Connected");
-        DataInputStream daInputStream = new DataInputStream(clientSocket.getInputStream());
-        DataOutputStream daOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+        DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 
         FileOutputStream fileOutputStream = new FileOutputStream(file);
-        long filesize = daInputStream.readLong();
+        long filesize = dataInputStream.readLong();
         byte[] buffer = new byte[4 * 1024];
-        int bytes = daInputStream.read(buffer, 0, (int) Math.min(buffer.length, filesize));
-        while(filesize>0 && bytes!=-1){
+        int bytes = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, filesize));
+
+        while(filesize > 0){
             fileOutputStream.write(buffer,0,bytes);
             filesize-=bytes;
         }
-fileOutputStream.close();
-daInputStream.close();
-daOutputStream.close();
-clientSocket.close();
+
+        fileOutputStream.close();
+        dataInputStream.close();
+        dataOutputStream.close();
+        clientSocket.close();
     }
 }
